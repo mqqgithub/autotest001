d81cf65fad39631c3d1e210811f5b3b51fd4173e
mqqgithub <maqq1234>
2019-07-08 06:36:12 +0800

    webdriverTest

--- seleniumTest/webdriverTest.py
+++ seleniumTest/webdriverTest.py
@@ -1,5 +1,22 @@
+'''
+
+'''
+
+
+
 from selenium import webdriver
 
+class openbrowser():
+    def openChrome(self):
+        driver = webdriver.Chrome()
+        return  driver
+    def openFirefox(self):
+        driver = webdriver.Firefox()
+        return driver
+    def openIe(self):
+        driver = webdriver.Ie()
+        return driver
 
-driver = webdriver.Chrome()
-driver.get()
+if __name__=='__main__':
+    browser = openbrowser().openChrome()
+    browser.get('https://www.baidu.com')
