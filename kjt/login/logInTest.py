9ff028ec214e1fa84592aed606ca162fd01e899d
mqqgithub <756738633@qq.com>
2019-07-10 17:23:10 +0800

    test

--- kjt/login/logInTest.py
+++ kjt/login/logInTest.py
@@ -2,7 +2,20 @@
 
 '''
 import unittest
-class logintest(unittest.testcase):
-    def setup(self):
-        
+from kjt.login import logIn
+from kjt.login import openBro
+class logintest(unittest.TestCase):
+    def setUp(self):
+        print("start")
+        self.driver = openBro.openbroswer()
+    def tearDown(self):
+        print("ending")
+        self.driver.close()
+    def test_login_001(self):
+        logIn.login().login(self.driver, "maqingqing", "kjt@1231")
+
+
+if __name__=="__main__":
+    unittest.main(verbosity=2)
+
 
--- seleniumTest/alertTest.py
+++ seleniumTest/alertTest.py
@@ -43,3 +43,72 @@
     </script>
 </html>
 '''
+#-*-coding:utf-8 -*-
+import time
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get('file:///D:/autotest001/seleniumTest/alertTest.html')
+'''获取alert对话框的按钮,点击按钮,弹出alert对话框'''
+driver.find_element_by_xpath('/html/body/div/input[2]').click()
+'''获取alert对话框'''
+alert = driver.switch_to_alert()
+'''添加等待时间'''
+time.sleep(2)
+'''获取警告对话框的内容'''
+print (alert.text)  #打印警告对话框内容
+alert.accept()   #alert对话框属于警告对话框，我们这里只能接受弹窗
+'''添加等待时间'''
+time.sleep(2)
+
+##################################################
+'''获取confirm对话框的按钮,点击按钮,弹出confirm对话框'''
+driver.find_element_by_xpath('/html/body/div/input[3]').click()
+'''获取confirm对话框'''
+dialog_box = driver.switch_to_alert()
+'''添加等待时间'''
+time.sleep(2)
+'''获取对话框的内容'''
+#打印警告对话框内容
+print (dialog_box.text)
+'''点击【确认】显示"您为何如此自信？"'''
+dialog_box.accept()   #接受弹窗
+#打印接受对话框后的提示信息
+print (driver.find_element_by_xpath('//*[@id="textSpan"]/font').text)
+time.sleep(2)
+
+'''再次获取confirm对话框的按钮,点击按钮,弹出confirm对话框'''
+driver.find_element_by_xpath('/html/body/div/input[3]').click()
+'''再次获取confirm对话框'''
+dialog_box = driver.switch_to_alert()
+'''点击【取消】显示"您为何如此谦虚？"'''
+time.sleep(2)
+dialog_box.dismiss()  #关闭获取取消对话框
+print (driver.find_element_by_xpath('//*[@id="textSpan"]/font').text)
+##################################################33
+'''获取prompt对话框的按钮,点击按钮,弹出confirm对话框'''
+driver.find_element_by_xpath('/html/body/div/input[1]').click()
+'''获取prompt对话框'''
+dialog_box = driver.switch_to_alert()
+'''添加等待时间'''
+time.sleep(2)
+'''获取对话框的内容'''
+print (dialog_box.text)  #打印警告对话框内容
+dialog_box.send_keys("2")  #弹出框内输入2
+dialog_box.accept()  #接受
+print (driver.find_element_by_xpath('//*[@id="textSpan"]/font').text)  #获取关闭弹窗结果  #获取确认弹窗结果
+'''这里等待几秒在测试取消'''
+time.sleep(2)
+#************************点击【取消】,并且获取显示结果**********************
+driver.find_element_by_xpath('/html/body/div/input[1]').click()
+'''获取prompt对话框'''
+dialog_box = driver.switch_to_alert()
+'''添加等待时间'''
+time.sleep(2)
+dialog_box.dismiss()  #关闭对话框
+print (driver.find_element_by_xpath('//*[@id="textSpan"]/font').text)  #获取关闭弹窗结果
+time.sleep(2)
+
+
+
+driver.quit()
