c16dfb7753763849311b3adccb2febf287367292
mqqgithub <maqq1234>
2019-07-12 09:07:52 +0800

    webdriverTest

--- kjt/login/test.py
+++ kjt/login/test.py
@@ -2,10 +2,13 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.get("https://www.baidu.com")
+x = driver.find_element_by_xpath("//div/a[contains(text(), 新闻)]").text
+print(x)
 name = driver.find_element_by_css_selector("#kw")
 id = driver.find_element_by_css_selector("#su")
 elements = (name, id)
-elements[0].send_keys("java")
-elements[1].click()
+#elements[0].send_keys("java")
+#elements[1].click()
+print(1)
 
 
