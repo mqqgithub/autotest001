9af5ce4b372b96565b4b8904fa24bd1b30261c6a
mqqgithub <756738633@qq.com>
2019-08-12 18:43:34 +0800

    test1

--- seleniumTest/test001.py
+++ seleniumTest/test001.py
@@ -1,9 +1,8 @@
 #浏览器的相关操作
 from selenium import webdriver
-#
 #driver = webdriver.Chrome("D:\\TOOLS\jenkins\\browser_driver\\chromedriver.exe")
 driver = webdriver.Chrome("C:\\Users\\maqingqing\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe")
-
+#driver = webdriver.Chrome()
 driver.get("https://www.baidu.com")
 driver.maximize_window()
 driver.set_window_size(1000, 800)
@@ -11,5 +10,5 @@ driver.get("https://www.w3cschool.cn/")
 driver.back()
 driver.forward()
 driver.quit()
-driver.close()
+#driver.close()
 # close()用于关闭当前窗口，quit()用于退出驱动程序并关闭所有相关窗口。
