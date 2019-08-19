4f5bd26df742f36cb4e6df81db0e17ee8e2fb14f
mqqgithub <maqq1234>
2019-08-19 22:42:14 +0800

    webdriverTest

--- seleniumTest/test.py
+++ seleniumTest/test.py
@@ -3,6 +3,4 @@ from selenium import webdriver
 d = webdriver.Chrome()
 d.get("https://www.baidu.com")
 d.maximize_window()
-d.find_element_by_css_selector("input[class^='s']").send_keys("python")
-#d.find_element_by_css_selector("input[class~='bg']").click()
-d.find_element_by_css_selector("#su").submit()
+d.find_element_by_xpath("//input[contains(@id,'kw')]").send_keys("python")
