b9aacbd67d6863344dd86890174e055f4d2d2416
mqqgithub <maqq1234>
2019-10-21 09:00:31 +0800

    webdriverTest

---
+++ pythonStudy/T009_os文件路径操作.py
@@ -0,0 +1,18 @@
+import os
+
+# 获取操作系统
+print(os.name)
+# 获取环境变量
+print(os.environ.get('path'))
+# 获取当前的绝对路径
+print(os.path.abspath(','))
+# 路径拼接 在某个目录下创建一个新目录，首先把新目录的完整路径表示出来
+os.path.jion('', '')
+# 创建一个文件夹
+os.mkdir('')
+# 删除一个文件夹
+os.rmdir()
+# 拆分路径，把最后一个去掉
+os.path.split('')
+# 删除文件
+os.remove('')
---
+++ pythonStudy/T010_编码问题.py
@@ -0,0 +1,1 @@
+# 参考此文章https://www.cnblogs.com/vipchenwei/p/6993788.html
