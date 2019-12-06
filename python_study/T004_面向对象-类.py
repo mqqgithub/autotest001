a190ebd401335cfd3e9f25d6f71ebc11d938a205
mqqgithub <756738633@qq.com>
2019-12-06 18:55:56 +0800

    test1

--- python_study/T004_面向对象-类.py
+++ python_study/T004_面向对象-类.py
@@ -9,18 +9,21 @@ class Student():
     # 第一种方法__init__()方法是一种特殊的方法，被称为类的构造函数或初始化方法，当创建了这个类的实例时就会调用该方法
     def __init__(self, name, age):
         # 构造函数
-        # 初始化变量的属性 ，实例变量、局部变量
+        # 初始化对象的属性 ，实例变量、局部变量
         self.name = name
         self.age = age
+
 
 student1 = Student("王", 2)
 print(student1.name)
 print(Student.name)
-###通过这个例子，能看出来实例化的时候，类变量并没有改变。self代表的是当前的实例，所以和self.变量总是实例变量
+# 通过这个例子，能看出来实例化的时候，类变量并没有改变。self代表的是当前的实例，所以和self.变量总是实例变量
 
 # 类的私有属性:__private_attrs：两个下划线开头，声明该属性为私有，不能在类的外部被使用或直接访问。在类内部的方法中使用时 self.__private_attrs。
 #     类的方法:在类的内部，使用 def 关键字可以为类定义一个方法，与一般函数定义不同，类方法必须包含参数 self,且为第一个参数
 # 类的私有方法:private_method：两个下划线开头，声明该方法为私有方法，不能在类的外部调用。在类的内部调用 self.__private_methods
+
+
 class JustCounter:
     __secretCount = 0  # 私有变量
     publicCount = 0  # 公开变量
