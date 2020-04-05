a32bb57b9f1b7a1923c5f017326434fb3eba8991
mqqgithub <maqq1234>
2020-04-05 22:27:47 +0800

    webdriverTest

--- edu_python/T027_HTMLReport多线程.py
+++ edu_python/T027_HTMLReport多线程.py
@@ -2,7 +2,7 @@ import HTMLReport
 import unittest
 import os, time, sys
 
-# sys.path.append()
+# sys.path.append()增加模块查找路径相当于环境变量
 
 # 测试用例文件路径
 # test_dir = os.path.join(os.getcwd(), 'edu_python')
@@ -12,14 +12,14 @@ print('discover', discover)
 
 # 测试报告文件路径
 # 文件地址必须是同级或者是下一级
-report_path = 'D:\\autotest001'
+report_path = r'D:\autotest001\report'
 report_time = time.strftime("%Y_%m_%d_%H_%M_%S_")
-report_name = report_path+report_time + "report.html"
+report_name = report_time + "report.html"
 
 
 def run():
     runner = HTMLReport.TestRunner(title="测试报告", description='测试deafult报告', output_path=report_path,
-                                   report_file_name=report_name, thread_count=2)
+                                   report_file_name=report_name, thread_count=6)
     runner.run(discover)
 
 
@@ -27,4 +27,6 @@ if __name__ == '__main__':
     run()
 
 
-
+'''
+多线程是模块级别的，即每个线程执行一个模块，适合执行接口测试用例，如果要UI测试的话，还需要分布式
+'''
