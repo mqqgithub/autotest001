0a5bc161dfc4b49a5c4465278d84bbc3d5940db2
maqingqing <756738633@qq.com>
2020-05-27 18:56:17 +0800

    11

--- edu_selenium/test021_webdriver原理.py
+++ edu_selenium/test021_webdriver原理.py
@@ -44,3 +44,13 @@ url = 'http://localhost:9515/session/'+response_session.json()['sessionId']+'/ur
 value = {"url": "https://www.cnblogs.com/linuxchao/", "sessionId": response_session.json()['sessionId']}
 response_blog = requests.post(url = url,json = value)
 print(response_blog.json())
+
+
+from selenium import webdriver
+import logging
+logging.basicConfig(level=logging.DEBUG)
+diver = webdriver.Chrome()
+diver.get("http://www.baidu.com")
+diver.find_element_by_id("kw").send_keys("selenium")
+diver.find_element_by_id("su").click()
+diver.quit()
