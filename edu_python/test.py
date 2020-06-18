60a48029670f80c0c0814a7199ca5baa7e1aa534
maqingqing <756738633@qq.com>
2020-06-18 18:34:37 +0800

    11

--- edu_python/test.py
+++ edu_python/test.py
@@ -1,4 +1,6 @@
-dic = {'a': 1, 'b': 2}
-print(dic.items())
-
-
+try:
+    a = input("输入一个数：")
+    if(not a.isdigit()):
+        raise Exception("a 必须是数字")
+except Exception as e:
+    print("引发异常1：", e)
