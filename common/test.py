17d92a9da64ffc48a28225095ba54b624f6def5e
maqingqing <756738633@qq.com>
2020-07-15 19:17:53 +0800

    11

--- common/test.py
+++ common/test.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get('http://www.baidu.com')
-ele = WebDriverWait(driver).until(EC.visibility_of_element_located())
+driver.find_element_by_id("kw").send_keys("aa")
+el = WebDriverWait(driver, 10, 0.5).until(EC.visibility_of_element_located((By.ID, "su")))
+el.click()
+
+
