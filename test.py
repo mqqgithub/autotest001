a37dc4454469fc312e553ae8a8bef614c949742c
mqqgithub <maqq1234>
2020-08-01 21:18:25 +0800

    webdriverTest

--- test.py
+++ test.py
@@ -14,3 +14,13 @@ class A():
 a = A()
 b = A()
 print(id(a), id(b))
+
+a = [1, 3, 10, 9, 21, 35, 4, 6]
+s = range(1, len(a))[::-1]
+print(list(s)) # 交换次数
+for i in s:
+    for j in range(i):
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+    print("第 %s 轮交换后数据：%s" % (len(s)-i+1, a))
+print(a)
