7b4f14b7c7559d5568a0455e6a6bd134f7734d69
maqingqing <756738633@qq.com>
2020-08-06 18:41:33 +0800

    11

--- common/test.py
+++ common/test.py
@@ -1,6 +1,5 @@
-import random
-
-a = 'mqq_'+''.join(random.sample('123456abc', 5))+'@163.com'
-print(a)
-b = random.randint(1,100000)
-print(b)
+print('extend  迭代着追加，在列表的最后面迭代着追加一组数据')
+l = [1, 2, 'a']
+l.extend('太白a')
+l.extend(['q', 'w', 'e'])
+print(l)
